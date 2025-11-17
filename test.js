0d2c5961d344c37c447fdf568817be7d30fcc193
JoeNokx <146049841+JoeNokx@users.noreply.github.com>
2025-11-17 21:32:51 +0000

    Add files via upload
    this file is found in config

---
+++ test.js
@@ -0,0 +1,69 @@
+const larvis ={
+  "fullName": "Larvis Blessing",
+  "email": "larvis@example.com",
+  "phoneNumber": "05460225643",
+  "password": "larvis0012345",
+  "role": "",
+  "churchName": "Grace-Life Church"
+}
+
+
+const giles = {
+  "fullName": "Giles Sekyi",
+  "email": "giles@example.com",
+  "phoneNumber": "05434225643",
+  "password": "giless0012345",
+  "role": "churchadmin",
+  "churchName": "God is our Light Church"
+}
+
+const edward = {
+  "fullName": "Edwark K. Yalley",
+  "email": "edward@example.com",
+  "phoneNumber": "05435125643",
+  "password": "edward0012345",
+  "role": "churchadmin",
+  "churchName": "Kingdom Labourers Ministry"
+}
+
+const superadmin = {
+  "fullName": "Clement Quaye",
+  "email": "clement@example.com",
+  "phoneNumber": "05435755643",
+  "password": "clement0012345",
+  "role": "superadmin"
+}
+
+const binnah =  {
+  "fullName": "Joseph Binnah",
+  "email": "joseph@example.com",
+  "phoneNumber": "05035125643",
+  "password": "joeseph0012345",
+  "role": "churchadmin",
+  "churchName": "Grace Connect"
+}
+
+
+
+// {
+//     "message": "Event created successfully.",
+//     "event": {
+//         "church": "6918a14719640edd8f724a32",
+//         "title": "Grace Life Musice",
+//         "category": "Worship",
+//         "description": "program hosted by Life Celebrant.",
+//         "dateFrom": "2025-12-24T00:00:00.000Z",
+//         "time": "16:00",
+//         "venue": "Mighty Auditorium, Apowa",
+//         "organizers": "Ann Eshun",
+//         "createdBy": "6918a14719640edd8f724a34",
+//         "_id": "6919d84166a64e15dea245d8",
+//         "attendees": [],
+//         "createdAt": "2025-11-16T13:57:21.889Z",
+//         "updatedAt": "2025-11-16T13:57:21.889Z",
+//         "__v": 0
+//     }
+// }
+
+
+export {larvis, giles, edward};
